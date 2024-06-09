702c981a4ea703bbb40e38cfe01f44d5758867c9
Zorica Vuković <90617418+zoricavukovic@users.noreply.github.com>
2024-06-09 10:59:11 +0200

    feat: get and update notification settings (#2)

--- notification-service/application/notification-settings-service.go
+++ notification-service/application/notification-settings-service.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"github.com/mmmajder/zms-devops-notification-service/domain"
+	"github.com/mmmajder/zms-devops-notification-service/infrastructure/dto"
 	"net/http"
 )
 
@@ -17,6 +18,33 @@ func NewNotificationSettingsService(store domain.UserNotificationSettingsStore, 
 	}
 }
 
+func (service *NotificationSettingsService) Update(userId string, userRole string, settingsRequest []domain.NotificationSetting) error {
+	settings, err := service.store.GetByUserId(userId)
+	if err != nil {
+		return err
+	}
+	if userRole == domain.HostRole {
+		settings.Settings = service.excludeNotificationSettings(settingsRequest, domain.ReviewReservation)
+	} else {
+		settings.Settings = service.filterNotificationSettings(settingsRequest, domain.ReviewReservation)
+	}
+	err = service.store.Update(settings.Id, settings)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (service *NotificationSettingsService) Get(userId string) (*[]dto.NotificationSettingDTO, error) {
+	settings, err := service.store.GetByUserId(userId)
+	if err != nil {
+		return nil, err
+	}
+
+	return dto.FromUserNotificationSettings(settings), nil
+}
+
 func (service *NotificationSettingsService) Delete(userId string) error {
 	err := service.store.DeleteByUserId(userId)
 	if err != nil {
@@ -24,4 +52,28 @@ func (service *NotificationSettingsService) Delete(userId string) error {
 	}
 
 	return nil
+}
+
+func (service *NotificationSettingsService) filterNotificationSettings(settings []domain.NotificationSetting, notificationType domain.NotificationType) []domain.NotificationSetting {
+	var filteredSettings []domain.NotificationSetting
+
+	for _, setting := range settings {
+		if setting.Type == notificationType {
+			filteredSettings = append(filteredSettings, setting)
+		}
+	}
+
+	return filteredSettings
+}
+
+func (service *NotificationSettingsService) excludeNotificationSettings(settings []domain.NotificationSetting, notificationType domain.NotificationType) []domain.NotificationSetting {
+	var filteredSettings []domain.NotificationSetting
+
+	for _, setting := range settings {
+		if setting.Type != notificationType {
+			filteredSettings = append(filteredSettings, setting)
+		}
+	}
+
+	return filteredSettings
 }
